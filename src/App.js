2718ac47f2bbc1621642c6222a790a7ea79021c4
Richard Tan <richard.tan@crownbet.com.au>
2016-12-20 21:20:57 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BinarySearch from './components/BinarySearch/BinarySearch';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <BinarySearch />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/BinarySearch/BinarySearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './BinarySearch.scss';
+
+class BinarySearch extends Component {
+  render() {
+    return (
+      <div className="binary">
+        binary search
+      </div>
+    );
+  }
+}
+
+export default BinarySearch;
