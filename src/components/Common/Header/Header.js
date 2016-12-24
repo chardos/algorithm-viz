a9587c82591ec1dd110bc7d7975ae8e16429b317
Richard Tan <richard.tan@crownbet.com.au>
2016-12-24 13:12:26 +1100

    Styling header

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.scss';
 export default function Header(props) {
     return (
         <div className="header">
-            <Link to="/binarysearch" className="header__link">Bubble sort</Link>
+            <Link to="/binarysearch" className="header__link">Binary search</Link>
             <Link to="/bubblesort" className="header__link">Bubble sort</Link>
         </div>
     );
