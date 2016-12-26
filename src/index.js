a4e62efc39f260c71f709e2c540607aab3003356
Richard Tan <richard.tan@crownbet.com.au>
2016-12-26 19:39:17 +1100

    Removed unused imports in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './index.scss';
 
