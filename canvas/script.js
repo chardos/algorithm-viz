f4d93deab92858bc5abb482b48bb29bc8048b7ed
Richard Tan <richard.tan@crownbet.com.au>
2017-01-19 23:19:48 +1100

    Got repeating diamonds

--- canvas/script.js
+++ canvas/script.js
@@ -5,17 +5,46 @@ console.log(canvas);
 // canvas.width  = windowWidth;
 // canvas.height = windowHeight;
 var ctx = canvas.getContext('2d');
+const widthMod = 0.722;
 
-function drawDiamond(size, ctx){
+function drawDiamond(size, lineWidth, ctx){
     const centerX = ctx.canvas.width / 2;
     const centerY = ctx.canvas.height / 2;
+    const top = centerY - size;
+    const left = centerX - size * 0.722;
+    const right = centerX + size * 0.722;
+    const bottom = centerY + size;
 
-    ctx.moveTo(centerX, centerY);
-    ctx.lineTo(centerX-50,centerY-50);
+    ctx.moveTo(centerX, top);
+    ctx.lineWidth = lineWidth;
+    ctx.lineTo(right, centerY);
+    ctx.lineTo(centerX, bottom);
+    ctx.lineTo(left, centerY);
+    ctx.closePath()
+    // ctx.lineTo(centerX, top);
     ctx.stroke();
 }
 
-ctx.beginPath();
-drawDiamond(5, ctx);
-console.log(ctx);
-console.log(ctx.canvas.width);
+function hasDiamondReachedEdge(size, ctx){
+    if(
+        size > ctx.canvas.height &&
+        size * widthMod > ctx.canvas.width
+    ){
+        return true;
+    }
+    return false;
+}
+
+function step(size, ctx){
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+        drawDiamond(size, 30, ctx);
+
+    if(hasDiamondReachedEdge(size, ctx)){
+        requestAnimationFrame( () => { step(0, ctx)} )
+    }
+    else {
+        requestAnimationFrame( () => { step(size+10, ctx)} )
+    }
+}
+step(100, ctx);
